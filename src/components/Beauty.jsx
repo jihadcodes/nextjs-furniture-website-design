f4b8283661c5265a0f2e3e9ac49acf63a7c973ea
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-07 01:25:58 +0600

    cheaking

--- src/components/Beauty.jsx
+++ src/components/Beauty.jsx
@@ -5,7 +5,7 @@ import girl from '../../public/image/girl.png'
 const Beauty = () => {
   return (
     <>
-    <section className='bg-[#F2F5FF] pb-15'>
+    <section className='bg-[#F2F5FF] pb-15  '>
         <div className="container">
               <div className="flex gap-15.5">
                         <div className=" w-128.5 pt-73.75">
@@ -19,8 +19,9 @@ const Beauty = () => {
                                      LEARN MORE
                                 </button>
                         </div>
-                        <div className="b-right py-15">
-                            <Image src={girl} alt=''/>
+                        <div className="b-right py-15 relative">
+                            <Image src={girl} alt='' className=''/>
+                            {/* <div className=' w-93 h-93 rounded-full bg-[#09513B] absolute top-[50%] -translate-y-1/2 -right-10  '></div> */}
 
                         </div>
               </div>
